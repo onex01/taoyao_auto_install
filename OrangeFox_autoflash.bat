e92406c21c24f0cbd67cc60b05eedca89cbeec9f
onex01 <113294842+onex01@users.noreply.github.com>
2025-02-14 20:18:25 +0400

    Update OrangeFox_autoflash.bat

--- OrangeFox_autoflash.bat
+++ OrangeFox_autoflash.bat
@@ -7,7 +7,7 @@ set ADB_PATH=%~dp0adb/
 cd /d "%ADB_PATH%"
 set OFOX_PATH=%~dp0ofox/
 
-%ADB_PATH%/fastboot.exe boot %OFOX_PATH%/ofox.img || @echo "Error boot ro OrangeFox" && exie 1
+%ADB_PATH%/fastboot.exe boot %OFOX_PATH%/ofox.img || @echo "Error boot ro OrangeFox" && exit 1
 echo Wait for OrangeFox to load, then press enter, the process of copying ofox.zip to the /sdcard/ path will begin. from it you flash the archive
 pause
 %ADB_PATH%/adb push %OFOX_PATH%/*.zip /sdcard/
