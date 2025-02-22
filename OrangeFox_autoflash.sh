c3a5f005553fb814427d8975b37ccbb9a77fd073
onex01 <113294842+onex01@users.noreply.github.com>
2025-02-22 22:43:01 +0400

    Update OrangeFox_autoflash.sh

--- OrangeFox_autoflash.sh
+++ OrangeFox_autoflash.sh
@@ -7,7 +7,7 @@ echo "-------------------------------------------"
 echo "                 By OneX01"
 echo "-------------------------------------------"
 
-fastboot $* boot `dirname $0`/ofox/*.img
+fastboot $* boot `dirname $0`/ofox/ofox.img
 if [ $? -ne 0 ] ; then echo "Error boot to OrangeFox"; exit 1; fi
 
 echo "Wait for OrangeFox to load, then press enter, the process of copying ofox.zip to the /sdcard/ path will begin. from it you flash the archive"
