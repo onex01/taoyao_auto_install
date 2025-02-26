812706273fe81bcdf77ae9d5f461af202da0dc34
onex01 <113294842+onex01@users.noreply.github.com>
2025-02-26 21:00:11 +0400

    Update OrangeFox_autoflash.sh

--- OrangeFox_autoflash.sh
+++ OrangeFox_autoflash.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
-# Find all .zip files in the 'rom' directory
-ROM_DIR="$(dirname $0)/rom"
-ZIP_FILES=()
-for file in $(find "$ROM_DIR" -maxdepth 1 -name '*.zip'); do
-    ZIP_FILES+=("${file##*/}")
-done
-
-# Function to display menu and get user choice
-select_option() {
-    PS3="Please select an option: "
-    options=("$@")
-    select opt in "${options[@]}"; do
-        if [ "$opt" ]; then
-            echo "You selected: $opt"
-            return
-        else
-            echo "Invalid selection. Please try again."
-        fi
-    done
-}
+# It is recommended to use bash instead of sh.
+# Because the last command of installation will give an error.
 
 echo "-------------------------------------------"
-echo "			OneX01"
+echo "                 By OneX01"
 echo "-------------------------------------------"
 
-# Choose the ZIP file
-echo "Available .zip files in '$ROM_DIR':"
-select_option "${ZIP_FILES[@]}"
-CHOSEN_ZIP_FILE=$opt
+fastboot $* boot `dirname $0`/ofox/ofox.img
+if [ $? -ne 0 ] ; then echo "Error boot to OrangeFox"; exit 1; fi
 
-# Pause to enable sideload
-echo "Press ENTER to start seideload the ROM..."
-read -n 1 -s -r # Use bash.
+echo "Wait for OrangeFox to load, then press enter, the process of copying ofox.zip to the /sdcard/ path will begin. from it you flash the archive"
+read
 
-# adb line 46-47
-adb $* sideload "$ROM_DIR/$CHOSEN_ZIP_FILE"
-if [ $? -ne 0 ] ; then echo "Send rom error"; exit 1; fi
-echo "ROM installed successfully :D!"
+adb $* push `dirname $0`/ofox/ofox.zip /sdcard/
